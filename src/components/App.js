25a22550a716369838450ac8b4f625d97c3dfa9c
kadam-pallavi <117823209+kadam-pallavi@users.noreply.github.com>
2023-03-03 15:41:12 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,53 +1,33 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    constructor(props){
-        super(props);
-        this.state={
-            time : new Date(),
-           
-        }
-        this.intervalId = null;
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      time: new Date().toLocaleString(),
+    };
+  }
 
-    gettimeString(){
-        const[hours,minutes,seconds]=[this.state.time.getHours(),this.state.time.getMinutes(),this.state.time.getSeconds()];
-        const amorpm = hours>=12 ? "PM" :"AM";
-        const twelvehour = hours >12 ? hours-12 :hours;
-        const hourstring = ""+twelvehour;
-        const minutestring = this.paddednum(minutes);
-        const secondstring = this.paddednum(seconds);
-        const fstring = `${hourstring}:${minutestring}:${secondstring} ${amorpm}`
-        return fstring;
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      this.setState({
+        time: new Date().toLocaleString(),
+      });
+    }, 1000);
+  }
 
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
 
-    }
-    componentDidMount(){
-        this.intervalId = setInterval(()=>{
-            this.setState({
-                time:new Date()
-            }
-        )},1*1000);
-    }
-    componentWillUnmount(){
-        clearInterval(this.intervalId);
-    }
-    paddednum(num){
-      return ( num<10 ? ("0"+num) : (""+num));
-      
-
-    }
-    render() {
-
-        return(
-            <div className="Clock">
-                <h3 id="time">{this.gettimeString()}</h3>
-               
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div id="main">
+        <div className="date-time">{this.state.time}</div>
+      </div>
+    );
+  }
 }
-
 
 export default App;
